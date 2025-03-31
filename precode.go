f1facfe2b0d432933346c5a823bd79aac9c81530
ElderSoap <elder.soap1@gmail.com>
2025-03-31 05:48:23 +0300

    second iteration

--- precode.go
+++ precode.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"net/http"
-	"net/http/httptest"
 	"strconv"
 	"strings"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 var cafeList = map[string][]string{
@@ -47,29 +42,4 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(answer))
-}
-
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	handler := http.HandlerFunc(mainHandle)
-
-	// Тест корректного запроса
-	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
-	responseRecorder := httptest.NewRecorder()
-	handler.ServeHTTP(responseRecorder, req)
-	assert.Equal(t, http.StatusOK, responseRecorder.Code)
-	assert.NotEmpty(t, responseRecorder.Body.String())
-
-	// Тест неверного города
-	req = httptest.NewRequest("GET", "/cafe?count=2&city=paris", nil)
-	responseRecorder = httptest.NewRecorder()
-	handler.ServeHTTP(responseRecorder, req)
-	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
-	assert.Equal(t, "wrong city value", responseRecorder.Body.String())
-
-	// Тест, когда count больше доступного
-	req = httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
-	responseRecorder = httptest.NewRecorder()
-	handler.ServeHTTP(responseRecorder, req)
-	require.Equal(t, http.StatusOK, responseRecorder.Code)
-	assert.Equal(t, "Мир кофе,Сладкоежка,Кофе и завтраки,Сытый студент", responseRecorder.Body.String())
 }
